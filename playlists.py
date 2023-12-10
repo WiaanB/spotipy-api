1164a248130273c22a52726d9879b0a3a7f7d9aa
wiaan <wiaan.botha@spandigital.com>
2023-12-10 08:55:58 +0200

    Add playlist generation endpoint

--- playlists.py
+++ playlists.py
@@ -7,10 +7,20 @@ router = APIRouter()
 class PlaylistSearchBody(BaseModel):
     id: list[str]
 
+class PlaylistGenerateBody(BaseModel):
+    artists: list[str] | None = None
+    genres: list[str] | None = None
+
 @router.post("/get")
 def get_user(search_body: PlaylistSearchBody):
     sp = spotify_agent.create_spotify_instance()
     results = []
     for id in search_body.id:
         results.append(sp.playlist(id))
+    return {"data": results}
+
+@router.post('/generate')
+def generate_playlist(generate_body: PlaylistGenerateBody | None = None):
+    sp = spotify_agent.create_spotify_instance()
+    results = sp.recommendations(seed_artists=generate_body.artists, seed_genres=generate_body.genres)
     return {"data": results}
